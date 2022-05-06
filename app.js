1c0b9f97738baf80b5d14a5671407729073b3fba
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2022-05-07 01:02:08 +0700

    updated

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000,
@@ -30,8 +31,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// extra packages
 
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
+
+// routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
